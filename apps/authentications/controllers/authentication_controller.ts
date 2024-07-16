bb36fa1b61764447ea78f494a95af2fde97897b8
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-07-16 17:33:43 +0200

    feat: implement authentication

--- apps/authentications/controllers/authentication_controller.ts
+++ apps/authentications/controllers/authentication_controller.ts
@@ -4,7 +4,6 @@ import User from '#apps/accounts/models/user'
 export default class AuthenticationController {
   async login({ request }: HttpContext) {
     const { email, password } = request.only(['email', 'password'])
-
     const user = await User.verifyCredentials(email, password)
     const token = await User.accessTokens.create(user, ['*'], {
       expiresIn: '30 days',
